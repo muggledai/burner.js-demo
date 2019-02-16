39f83fe3973882333b554c9337112682524e5fed
rob shaw <rob@borwahs.com>
2019-02-16 13:07:28 -0700

    Make simple entry point with example for dev.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+/** React **/
+import React from 'react';
+import { render } from 'react-dom';
+
+/** Definitions **/
+const App = ({}) => {
+    return (
+        <div>burner.js</div>
+    );
+}
+/** Render to Page **/
+render(<App />, document.getElementById('app'));
